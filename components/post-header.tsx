b327afe8b63b1b5d517a229438671207d7508999
Cbot <79479940+cboteleven@users.noreply.github.com>
2023-10-10 19:07:54 -0700

    Fix on deployment failing.
    Deployments are failing due to a dev test on my end. This is implementing a fix.

--- components/post-header.tsx
+++ components/post-header.tsx
@@ -18,7 +18,7 @@ const PostHeader = ({ title, coverImage, date, author }: Props) => {
       <div className="hidden md:block md:mb-12">
         <Avatar name={author.name} picture={author.picture} />
       </div>
-      <div className="mb-8 md:mb-16 sm:mx-0 width-1300px height-630px" style="img { height: unset; }">
+      <div className="mb-8 md:mb-16 sm:mx-0 width-1300px height-630px">
         <CoverImage title={title} src={coverImage}/>
       </div>
       <div className="max-w-2xl mx-auto">
