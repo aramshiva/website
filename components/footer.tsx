85440b3051ae309af41004542490a65be4e1860f
Cbot <aramshiva@icloud.com>
2023-10-20 19:00:05 -0700

    adding some content to index
    added new credits updates and styling

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,11 +3,9 @@ import Container from './container'
 const Footer = () => {
   return (
     <footer>
-      <Container>
-        <div className="text-center static-bottom font-mono pt-40 pb-20">
-        <span>---- END OF TRANSMISSION ----</span>
-        </div>
-      </Container>
+      <div className="text-center static-bottom font-mono pt-40 pb-20">
+      <span>---- END OF TRANSMISSION ----</span>
+      </div>
     </footer>
   )
 }
