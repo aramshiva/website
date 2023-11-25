6f6707f1ce0fad758cfaa92a86aff7e586f94d63
aramshiva <aramshiva@icloud.com>
2023-11-25 10:40:00 -0800

    updated meta

--- components/meta.tsx
+++ components/meta.tsx
@@ -3,11 +3,14 @@ import Head from 'next/head'
 const Meta = () => {
   return (
     <Head>
-      <meta property="og:type" content="article"/>
+      <meta property="og:type" content="profile"/>
       <meta property="og:title" content="Aram Shiva"/>
       <meta property="og:url" content="https://aram.sh"/>
-      <meta property="og:image" content="https://cloud-5kwqzi9zg-hack-club-bot.vercel.app/0aramshiva.svg"/>
-      <meta property="og:description" content="Aram is a designer, developer and creator based in Seattle, Washington"/>
+      <meta property="og:image" content="https://cloud-itcmneqy0-hack-club-bot.vercel.app/0aramshiva.png"/>
+      <meta property="og:description" content="Aram is a student based in Seattle, Washington. He likes to develop, create and inspire."/>
+      <meta property="profile:first_name" content="Aram"/>
+      <meta property="profile:last_name" content="Shiva"/>
+      <meta property="profile:username" content="aramshiva"/>
     </Head>
   )
 }
