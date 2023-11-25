b60d9bf1d43fd384a85cc5967526fbc2115f7de8
aramshiva <aramshiva@icloud.com>
2023-11-25 10:48:09 -0800

    fixed preview images: the sequel

--- components/meta.tsx
+++ components/meta.tsx
@@ -6,7 +6,7 @@ const Meta = () => {
       <meta property="og:type" content="profile"/>
       <meta property="og:title" content="Aram Shiva"/>
       <meta property="og:url" content="https://aram.sh"/>
-      <meta property="og:image" content="/aramshiva.png"/>
+      <meta property="og:image" content="https://cloud-3ri5zqk55-hack-club-bot.vercel.app/0aramshiva.png"/>
       <meta property="og:description" content="Aram is a student based in Seattle, Washington. He likes to develop, create and inspire."/>
       <meta property="profile:first_name" content="Aram"/>
       <meta property="profile:last_name" content="Shiva"/>
