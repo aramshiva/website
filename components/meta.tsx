c45b75942e79f1c88cb7b80f49a50b7445c21313
aramshiva <aramshiva@icloud.com>
2023-11-25 10:33:30 -0800

    added meta

--- components/meta.tsx
+++ components/meta.tsx
@@ -1,42 +1,13 @@
 import Head from 'next/head'
-import { HOME_OG_IMAGE_URL } from '../lib/constants'
 
 const Meta = () => {
   return (
     <Head>
-      <link
-        rel="apple-touch-icon"
-        sizes="180x180"
-        href="/favicon/apple-touch-icon.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="32x32"
-        href="/favicon/favicon-32x32.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="16x16"
-        href="/favicon/favicon-16x16.png"
-      />
-      <link rel="manifest" href="/favicon/site.webmanifest" />
-      <link
-        rel="mask-icon"
-        href="/favicon/safari-pinned-tab.svg"
-        color="#000000"
-      />
-      <link rel="shortcut icon" href="/favicon/favicon.ico" />
-      <meta name="msapplication-TileColor" content="#000000" />
-      <meta name="msapplication-config" content="/favicon/browserconfig.xml" />
-      <meta name="theme-color" content="#000" />
-      <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
-      <meta
-        name="description"
-        content={`Aram.`}
-      />
-      <meta property="og:image" content={HOME_OG_IMAGE_URL} />
+      <meta property="og:type" content="article"/>
+      <meta property="og:title" content="Aram Shiva"/>
+      <meta property="og:url" content="https://aram.sh"/>
+      <meta property="og:image" content="https://cloud-5kwqzi9zg-hack-club-bot.vercel.app/0aramshiva.svg"/>
+      <meta property="og:description" content="Aram is a designer, developer and creator based in Seattle, Washington"/>
     </Head>
   )
 }
