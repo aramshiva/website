848fbae8eaa9a32807625ed65d766fa3dc97486e
aramshiva <aramshiva@icloud.com>
2023-11-26 15:31:15 -0800

    updated credits

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,8 +1,9 @@
+import Link from "next/link";
 export default function Example() {
   return (
     <>
-      <main className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
-        <div className="font-mono rounded-lg sticky w-fit text-black">
+      <main className="grid min-h-full place-items-center px-6 py-9 sm:py-20 lg:px-5 h-screen">
+        <div className="font-mono rounded-lg w-fit">
           <p>██╗&nbsp;&nbsp;██╗&nbsp;██████╗&nbsp;██╗&nbsp;&nbsp;██╗</p>
           <p>██║&nbsp;&nbsp;██║██╔═████╗██║&nbsp;&nbsp;██║</p>
           <p>███████║██║██╔██║███████║</p>
@@ -10,20 +11,21 @@ export default function Example() {
           <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;██║╚██████╔╝&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;██║</p>
           <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;╚═╝&nbsp;╚═════╝&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;╚═╝</p>
         </div>
-        <div className="text-center">
-          <p className="mt-6 text-base leading-7 text-gray-600 font-mono">
+        <div>
+          <p className="font-mono">
             Sorry, we couldn’t find the page you’re looking for.
           </p>
           <div className="mt-10 flex items-center justify-center gap-x-6">
-            <a
+            <Link
               href="/"
-              className="rounded-md bg-gray-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-gray-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 font-mono"
+              className="rounded-md bg-gray-300 px-3.5 py-2.5 text-sm font-semibold text-black shadow-sm hover:bg-gray-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 font-mono"
             >
               Go back home
-            </a>
+            </Link>
           </div>
         </div>
       </main>
+      <div className="rainbow h-6 sticky bottom-0" />
     </>
   );
 }
--- pages/credits.tsx
+++ pages/credits.tsx
@@ -1,67 +1,76 @@
 import Layout from "../components/layout";
 import Container from "../components/container";
 import Link from "next/link";
+import { Typewriter } from "react-simple-typewriter";
 
 export default function Index() {
   return (
     <>
-        <div>
-            <Layout>
-                <Container>
-                    <div className="font-sans link:font-black">
-                        <div className="font-black pt-20 text-5xl">
-                            <p>CREDITS</p>
-                        </div>
-                        <div>
-                            <div className="italic">
-                            "Good artists borrow, Great artists steal.”
-                            </div>
-                            <div>-Pablo Picasso 🎨</div>
-                        </div>
-                        <br />
-                        <br />
-                        <div>
-                            <div>
-                            <div>HELP</div>
-                            <div>
-                                <Link href="https://arashshiva.com" className="hover:uppercase hover:no-underline hover:font-mono">Arash Shiva</Link><br />
-                                <Link href="https://hackclub.com/slack" className="hover:uppercase hover:no-underline hover:font-mono">Fellow Hack Clubbers</Link>
-                            </div>
-                            </div>
-                            <br />
-                            <div>
-                            <p>INSPIRATION</p>
-                            </div>
-                            <div>
-                            <Link href="https://www.arashshiva.com/" className="hover:uppercase hover:no-underline hover:font-mono">arashshiva.com</Link>
-                            <br />
-                            <Link href="https://eliasruiz.com/#about" className="hover:uppercase hover:no-underline hover:font-mono">eliasruiz.com</Link>
-                            <br />
-                            <Link href="https://tr.af/" className="hover:uppercase hover:no-underline hover:font-mono">tr.af</Link>
-                            <br />
-                            <Link href="https://chronark.com/" className="hover:uppercase hover:no-underline hover:font-mono">chronark.com</Link>
-                            <br />
-                            <Link href="https://github.com/itsmingjie/monolith/tree/main" className="hover:uppercase hover:no-underline hover:font-mono">
-                                github/itsmingie/monolith
-                            </Link>
-                            <br />
-                            </div>
-                            <br />
-                            <div>
-                            <div className=" line-through">STOLEN CODE</div>
-                            <div>
-                                <Link href="https://github.com/vercel/next.js/tree/canary/examples/blog-starter" className="hover:uppercase hover:no-underline hover:font-mono">
-                                vercel/next.js/canary/examples/blog-starters
-                                </Link>
-                            </div>
-                            </div>
-                            <br />
-                            
-                    </div>
-                    </div>
-                </Container>
-            </Layout>
-        </div>
+      <div>
+        <Layout>
+          <Container>
+            <div className="font-mono whitespace-pre-wrap pt-20">
+              ${" "}
+              <Typewriter
+                words={["cat aram.sh/public/credits.txt"]}
+                loop={1}
+                typeSpeed={100}
+                deleteSpeed={100}
+                delaySpeed={1000}
+                // onLoopDone={}
+              />
+              <span className="invisible" id="credits">
+                <br />
+                CREDITS
+                <br />
+                It's important to credit your sources.
+                <br />
+                <br />
+                <br />
+                HELP
+                <br />
+                Helped with some code!
+                <br />
+                <br />
+                <Link href="https://arashshiva.com">Arash Shiva</Link>
+                <br />
+                <br />
+                <br />
+                INSPIRATION
+                <br />
+                <br />
+                <Link href="https://malted.dev/">malted.dev</Link>
+                <br />
+                <Link href="https://reeseric.ci/">reeseric.ci</Link>
+                <br />
+                <Link href="https://zachlatta.com/">zachlatta.com</Link>
+                <br />
+                <Link href="https://maxwofford.com">maxwofford.com</Link>
+                <br />
+                <Link href="https://mingjie.dev/">mingjie.dev</Link>
+                <br />
+                <Link href="https://tobyb.dev/">tobyb.dev</Link>
+                <br />
+                <Link href="https://sarthakmohanty.me/">sarthakmohanty.me</Link>
+                <br />
+                <Link href="https://arashshiva.com">arashshiva.com</Link>
+                <br />
+                <Link href="https://sampoder.com/">sampoder.com</Link>
+                <br />
+                <Link href="https://eliasruiz.com/">eliasruiz.com</Link>
+                <br />
+                <Link href="https://chronark.com/">chonark.com</Link>
+                <br />
+                <Link href="https://github.com/itsmingjie/monolith/tree/main">
+                    itsmingjie/monolith
+                </Link>
+                <br />
+                <Link href="https://tr.af">tr.af</Link>
+              </span>
+            </div>
+          </Container>
+        </Layout>
+      </div>
     </>
   );
 }
