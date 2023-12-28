558b676bc34047107fe4d6055b18bee9453c094a
aramshiva <me@aram.sh>
2023-12-28 15:17:36 -0800

    🏃 Sprint
    Additions:
    - Removed Testing `spotify.tsx` file.
    - Changed site to be white opposing the black
    - Updated designs of spotify widget
    - Updated switches to be faster thanks to @ashiva
    
    Co-Authored-By: Arash S. <4908663+ashiva@users.noreply.github.com>

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,11 +13,11 @@ export default function Index() {
   const { data } = useSWR("/api/spotify", fetcher);
   return (
     <>
-      <div className="bg-black">
+      <div>
         <Layout>
           <Container>
             <div className="h-20" />
-            <div className="justify-left pl-5 text-7xl font-semibold text-white">
+            <div className="text-7xl font-semibold">
               <span>
                 Aram is a{" "}
                 <span>
@@ -44,64 +44,52 @@ export default function Index() {
               {"  "}
               <p></p>
             </div>
-            <div className="text-white justify-left pl-5 flex-wrap text-xl">
-              This website is currently
-              <span className="text-yellow font-bold text-2xl"> WIP </span>.
-              Come back later.
+            <div className="fixed bottom-0 text-black w-screen h-30 pb-20 align-middle flex flex-col">
+              <div className="pb-3">
+                {data?.isPlaying && <p>Currently Listening to:</p>}
+              </div>
+              <Link
+                target="_blank"
+                rel="noopener noreferer"
+                href={
+                  data?.isPlaying
+                    ? data.songUrl
+                    : "https://open.spotify.com/user/w0irmxmq6rkqvfpu5sznvym1u?si=7d100c9a0f7744a8"
+                }
+                className={
+                  data?.isPlaying
+                    && "relative flex items-center p-6 space-x-4 transition-shadow rounded-md hover:shadow-md w-72 border"
+                }
+              >
+                <div className="w-16">
+                  {data?.isPlaying && (
+                    <img
+                      src={data?.albumImageUrl}
+                      alt={data?.album}
+                      className="rounded-md"
+                    />
+                  )}
+                </div>
+
+                <div className="flex-1">
+                  <p className="font-bold component">
+                    {data?.isPlaying && data.title}
+                  </p>
+                  <p className="text-xs font-dark">
+                    {data?.isPlaying && data.artist}
+                  </p>
+                </div>
+                <div className="absolute bottom-1.5 right-1.5">
+                  <SiSpotify size={20} color={"#1ED760"} />
+                </div>
+                <div className="absolute bottom-1.5 right-1.5">
+                  {data?.isPlaying && (
+                    <SiSpotify size={20} color={"#1ED760"} />
+                  )}
+                </div>
+              </Link>
             </div>
           </Container>
-          <div className="fixed bottom-0 text-white w-screen h-30 p-20 text-center align-middle flex flex-col items-center justify-center">
-            <div className="pb-3">
-              {data?.isPlaying ? <p>Currently Listening to:</p> : <div />}
-            </div>
-            <Link
-              target="_blank"
-              rel="noopener noreferer"
-              href={
-                data?.isPlaying
-                  ? data.songUrl
-                  : "https://open.spotify.com/user/w0irmxmq6rkqvfpu5sznvym1u?si=7d100c9a0f7744a8"
-              }
-              className={
-                data?.isPlaying
-                  ? "relative flex items-center p-5 space-x-4 transition-shadow rounded-md hover:shadow-md w-72 border"
-                  : ""
-              }
-              // className='relative flex items-center p-5 space-x-4 transition-shadow rounded-md hover:shadow-md w-72'
-            >
-              <div className="w-16">
-                {data?.isPlaying ? (
-                  <img
-                    // className="w-16 shadow-sm"
-                    src={data?.albumImageUrl}
-                    alt={data?.album}
-                  />
-                ) : (
-                  <div />
-                  // <SiSpotify size={64} color={'#1ED760'} />
-                )}
-              </div>
-
-              <div className="flex-1">
-                <p className="font-bold component">
-                  {data?.isPlaying ? data.title : ""}
-                </p>
-                <p className="text-xs font-dark">
-                  {data?.isPlaying ? data.artist : ""}
-                </p>
-              </div>
-              {/* <div className='absolute bottom-1.5 right-1.5'>
-                            <SiSpotify size={20} color={'#1ED760'} />
-                        </div> */}
-              <div className="absolute bottom-1.5 right-1.5">
-                {data?.isPlaying ? (
-                  <SiSpotify size={20} color={"#1ED760"} />
-                ) : (
-                  <div />
-                )}
-              </div>
-            </Link>
-          </div>
         </Layout>
       </div>
     </>
