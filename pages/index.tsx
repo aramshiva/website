f2c613ea0c55bcc4d67532ed568feb655f353631
aramshiva <me@aram.sh>
2023-12-28 18:41:12 -0800

    🎵 Music Widget now updated!
    Music Widget is now interactive with parallax tilt and more simplistic design.
    
    Thanks to @kcoderhtml for some design help!
    
    Co-Authored-By: Kieran Klukas <92754843+kcoderhtml@users.noreply.github.com>

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ import { Inter } from "next/font/google";
 import useSWR from "swr";
 import { SiSpotify } from "react-icons/si";
 import Link from "next/link";
+import Tilt from "react-parallax-tilt";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -44,50 +45,56 @@ export default function Index() {
               {"  "}
               <p></p>
             </div>
-            <div className="fixed bottom-0 text-black w-screen h-30 pb-20 align-middle flex flex-col">
-              <div className="pb-3">
-                {data?.isPlaying && <p>Currently Listening to:</p>}
-              </div>
-              <Link
-                target="_blank"
-                rel="noopener noreferer"
-                href={
-                  data?.isPlaying
-                    ? data.songUrl
-                    : "https://open.spotify.com/user/w0irmxmq6rkqvfpu5sznvym1u?si=7d100c9a0f7744a8"
-                }
-                className={
-                  data?.isPlaying
-                    && "relative flex items-center p-6 space-x-4 transition-shadow rounded-md hover:shadow-md w-72 border"
-                }
-              >
-                <div className="w-16">
-                  {data?.isPlaying && (
-                    <img
-                      src={data?.albumImageUrl}
-                      alt={data?.album}
-                      className="rounded-md"
-                    />
-                  )}
+            <div
+              className={
+                data?.isPlaying
+                  ? "fixed bottom-0 text-black h-30 pb-20 align-middle flex flex-col"
+                  : "invisible"
+              }
+            >
+              {" "}
+              <div className="border p-5 rounded-xl">
+                <div className="pb-3">
+                  {data?.isPlaying && <p>Currently Listening to:</p>}
                 </div>
+                <Tilt glareEnable>
+                  <Link
+                    target="_blank"
+                    rel="noopener noreferer"
+                    href={
+                      data?.isPlaying
+                        ? data.songUrl
+                        : "https://open.spotify.com/user/w0irmxmq6rkqvfpu5sznvym1u?si=7d100c9a0f7744a8"
+                    }
+                    className={
+                      data?.isPlaying &&
+                      "relative flex items-center p-6 space-x-4 transition-shadow rounded-2xl hover:shadow-md w-72"
+                    }
+                  >
+                    <div className="w-16">
+                      {data?.isPlaying && (
+                        <img
+                          src={data?.albumImageUrl}
+                          alt={data?.album}
+                          className="rounded-md"
+                        />
+                      )}
+                    </div>
 
-                <div className="flex-1">
-                  <p className="font-bold component">
-                    {data?.isPlaying && data.title}
-                  </p>
-                  <p className="text-xs font-dark">
-                    {data?.isPlaying && data.artist}
-                  </p>
-                </div>
-                <div className="absolute bottom-1.5 right-1.5">
-                  <SiSpotify size={20} color={"#1ED760"} />
-                </div>
-                <div className="absolute bottom-1.5 right-1.5">
-                  {data?.isPlaying && (
-                    <SiSpotify size={20} color={"#1ED760"} />
-                  )}
-                </div>
-              </Link>
+                    <div className="flex-1">
+                      <p className="font-bold component">
+                        {data?.isPlaying && data.title}
+                      </p>
+                      <p className="text-xs font-dark">
+                        {data?.isPlaying && data.artist}
+                      </p>
+                    </div>
+                    <div className="absolute bottom-2 right-2">
+                      <SiSpotify size={20} color={"#000000"}/>
+                    </div>
+                  </Link>
+                </Tilt>
+              </div>
             </div>
           </Container>
         </Layout>
