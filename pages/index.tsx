f860a19fe6bc8af99c8206596ee73e234a9bf576
Aram <me@aram.sh>
2024-01-20 22:34:16 -0800

    added more

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,35 @@
-import Container from "../components/container";
-import { Typewriter } from "react-simple-typewriter";
-import Layout from "../components/layout";
-import { Inter } from "next/font/google";
-import SpotifyWidget from "../components/spotifynowplaying";
 import Link from "next/link";
-
-const inter = Inter({ subsets: ["latin"] });
-
-export default function Index() {
+export default function Error() {
   return (
     <>
-      <div>
-        <Layout>
-          <div className="bg-black h-screen w-screen text-white">
-            <p className="origin-center place-items-center justify-center text-center items-center align-center content-center object-center">Coming Soon</p>
+      <main className="grid min-h-full place-items-center px-6 py-9 sm:py-20 lg:px-5 h-screen">
+        <div className="font-mono rounded-lg w-fit">
+          <p>██╗&nbsp;&nbsp;██╗&nbsp;██████╗&nbsp;██╗&nbsp;&nbsp;██╗</p>
+          <p>██║&nbsp;&nbsp;██║██╔═████╗██║&nbsp;&nbsp;██║</p>
+          <p>███████║██║██╔██║███████║</p>
+          <p>╚════██║████╔╝██║╚════██║</p>
+          <p>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;██║╚██████╔╝&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;██║
+          </p>
+          <p>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;╚═╝&nbsp;╚═════╝&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;╚═╝
+          </p>
+        </div>
+        <div>
+          <p className="font-mono">
+            Sorry, we couldn’t find the page you’re looking for.
+          </p>
+          <div className="mt-10 flex items-center justify-center gap-x-6">
+            <Link
+              href="/"
+              className="rounded-md bg-gray-300 px-3.5 py-2.5 text-sm font-semibold text-black shadow-sm hover:bg-gray-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 font-mono"
+            >
+              Go back home
+            </Link>
           </div>
-          <SpotifyWidget />
-        </Layout>
-      </div>
+        </div>
+      </main>
+      <div className="rainbow h-6 sticky bottom-0" />
     </>
   );
 }
