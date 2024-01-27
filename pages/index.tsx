5c2ba98b84d19d8fe7c918982b6cc34889ca298c
Aram <me@aram.sh>
2024-01-26 17:34:57 -0800

    added weekend checker

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import { formatDistanceToNow } from "date-fns";
+import { formatDistanceToNow, isFriday } from "date-fns";
 import { Typewriter } from "react-simple-typewriter";
 import SpotifyWidget from "../components/spotifynowplaying";
 import Link from "next/link";
@@ -6,6 +6,7 @@ import { Tooltip } from "@material-tailwind/react";
 
 export default function Index() {
   const commitDate = new Date(process.env.COMMIT_DATE);
+  const isitweekend = isFriday(commitDate)
   const formattedCommitDate = formatDistanceToNow(commitDate, {
     addSuffix: true,
   });
@@ -66,7 +67,7 @@ export default function Index() {
                   {process.env.COMMIT_HASH}
                 </Tooltip>
               </a>{" "}
-              from {formattedCommitDate}
+              from {isitweekend ? "on a weekend" : formattedCommitDate}
             </span>
           </div>
         </div>
