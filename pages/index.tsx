d9489c21aaaa86a8d29fd62d28f67cb709173b92
Aram <me@aram.sh>
2024-01-27 15:38:04 -0800

    prettier

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,44 +8,42 @@ export default function Index() {
     <>
       <Nav />
       <div>
-        <div>
-          <div className="block text-sm sm:text-lg">
-            <span className="block text-xl font-semibold italic sm:text-3xl">
-              When did the internet get so{" "}
-              <span className="font-black">busy?</span>
-            </span>
-            Advertisements, pop-ups, notifications, trackers, spam. <br />
-            <br />I{"'"}m{" "}
-            <Link
-              className="underline decoration-blue-300 decoration-4 underline-offset-0"
-              href="https://github.com/aramshiva"
-            >
-              Aram
-            </Link>
-            , I don{"'"}t believe we{"'"}ve met.
-            <br />I craft web experiences to help us{" "}
-            <span className="font-bold">slow down</span> and appericate the{" "}
-            <span className="italic">little things.</span>
-            <br />
-            The world is changing so <span className="font-bold">fast</span>,
-            let{"'"}s take a moment to take a breather. <br />
-            This is my home on the interwebs, where I share my thoughts and
-            creations.
-            <br />
-            <br />
-            Sincerely, <br />
-            <br />
-            <Image
-              src="/signature.svg"
-              className="p-1"
-              alt="Aram Shiva"
-              width={100}
-              height={100}
-            />
-          </div>
+        <div className="block text-sm sm:text-lg">
+          <span className="block text-xl font-semibold italic sm:text-3xl">
+            When did the internet get so{" "}
+            <span className="font-black">busy?</span>
+          </span>
+          Advertisements, pop-ups, notifications, trackers, spam. <br />
+          <br />I{"'"}m{" "}
+          <Link
+            className="underline decoration-blue-300 decoration-4 underline-offset-0"
+            href="https://github.com/aramshiva"
+          >
+            Aram
+          </Link>
+          , I don{"'"}t believe we{"'"}ve met.
+          <br />I craft web experiences to help us{" "}
+          <span className="font-bold">slow down</span> and appericate the{" "}
+          <span className="italic">little things.</span>
+          <br />
+          The world is changing so <span className="font-bold">fast</span>, let
+          {"'"}s take a moment to take a breather. <br />
+          This is my home on the interwebs, where I share my thoughts and
+          creations.
+          <br />
+          <br />
+          Sincerely, <br />
+          <br />
+          <Image
+            src="/signature.svg"
+            className="p-1"
+            alt="Aram Shiva"
+            width={100}
+            height={100}
+          />
         </div>
-        <SpotifyWidget />
       </div>
+      <SpotifyWidget />
       {/* <CommitHash /> */}
     </>
   );
