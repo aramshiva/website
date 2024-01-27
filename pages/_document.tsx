722782b655d05a0033be0181ae33752d8b805c2c
Aram <me@aram.sh>
2024-01-26 20:44:52 -0800

    remove meta on aramshvia.com

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,4 @@
 import { Html, Head, Main, NextScript } from "next/document";
-import Meta from "../components/meta";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Analytics } from "@vercel/analytics/react";
 
@@ -11,7 +10,6 @@ export default function Document() {
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
         />
-        <Meta />
       </Head>
       <body>
         <Main />
