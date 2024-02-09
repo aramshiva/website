ace357890778c851fa9f1f592f13837d26ec4df3
Aram <me@aram.sh>
2024-02-08 21:49:12 -0800

    optimizations

--- pages/text.tsx
+++ pages/text.tsx
@@ -10,14 +10,11 @@ const MessagePage: React.FC<MessagePageProps> = ({ error }) => {
    useEffect(() => {
       let timeoutId: NodeJS.Timeout;
 
-      if (error) {
-         setDisplayedMessage(`Error: ${error}`);
-         return;
-      }
+      const fetchMessage = async () => {
+         try {
+            const response = await fetch("/message.txt");
+            const message = await response.text();
 
-      fetch("/message.txt")
-         .then((response) => response.text())
-         .then((message) => {
             let index = 0;
 
             const typeLetter = () => {
@@ -31,35 +28,42 @@ const MessagePage: React.FC<MessagePageProps> = ({ error }) => {
             };
 
             typeLetter();
-         })
-         .catch((error) => {
+         } catch (error) {
             console.error("Failed to fetch message:", error);
             setDisplayedMessage("Failed to fetch message");
-         });
+         }
+      };
+
+      if (error) {
+         setDisplayedMessage(`Error: ${error}`);
+      } else {
+         fetchMessage();
+      }
 
       return () => clearTimeout(timeoutId);
    }, [error]);
 
-   if (error) {
-      return <div>{displayedMessage}</div>;
-   }
+   useEffect(() => {
+      if (!error && displayedMessage.includes("{greeting}")) {
+         const fetchGreeting = async () => {
+            try {
+               const response = await fetch("/greeting.txt");
+               const greetings = await response.text();
+               const lines = greetings.split("\n");
+               const randomLine = lines[Math.floor(Math.random() * lines.length)];
+               const updatedMessage = displayedMessage.replace(
+                  "{greeting}",
+                  randomLine,
+               );
+               setDisplayedMessage(updatedMessage);
+            } catch (error) {
+               console.error("Failed to fetch greetings:", error);
+            }
+         };
 
-   if (displayedMessage.includes("{greeting}")) {
-      fetch("/greeting.txt")
-         .then((response) => response.text())
-         .then((greetings) => {
-            const lines = greetings.split("\n");
-            const randomLine = lines[Math.floor(Math.random() * lines.length)];
-            const updatedMessage = displayedMessage.replace(
-               "{greeting}",
-               randomLine,
-            );
-            setDisplayedMessage(updatedMessage);
-         })
-         .catch((error) => {
-            console.error("Failed to fetch greetings:", error);
-         });
-   }
+         fetchGreeting();
+      }
+   }, [error, displayedMessage]);
 
    return (
       <div className="h-screen w-screen whitespace-pre bg-black pl-2 pt-1 font-mono text-xs text-white">
