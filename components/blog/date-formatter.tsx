4014b3ab8aa66bec0ca27fb0657728f4ad9823ee
Aram <me@aram.sh>
2024-02-11 13:51:37 -0800

    Added Analytics using Redis and Upstash
    This push is for v9.5 adding analytics hosted using Redis and Upstash at /analytics. I used a tutorial from @joschan21 from the video https://www.youtube.com/watch?v=MGjCIQh5Pkw

--- components/blog/date-formatter.tsx
+++ components/blog/date-formatter.tsx
@@ -5,8 +5,8 @@ type Props = {
 };
 
 const DateFormatter = ({ dateString }: Props) => {
-   const date = dateString;
-   return <time dateTime={dateString}>{format(date, "LLLL	d, yyyy")}</time>;
+   const date = parseISO(dateString);
+   return <time dateTime={dateString}>{format(date, "LLLL d, yyyy")}</time>;
 };
 
 export default DateFormatter;
