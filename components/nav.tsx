c7fdb2cbfde943414b254550ca7867e4050d9b15
Aram <me@aram.sh>
2024-02-16 17:29:48 -0800

    Hid Guestbook
    Hid Guestbook for testing in prod.

--- components/nav.tsx
+++ components/nav.tsx
@@ -7,9 +7,9 @@ const navItems = {
    "/work": {
       name: "work",
    },
-   "/guestbook": {
-      name: "guestbook",
-   },
+   // "/guestbook": {
+   //    name: "guestbook",
+   // },
    "mailto:me@aram.sh": {
       name: "email",
    },
