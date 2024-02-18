b175a97520d0ac40b79b0b076e13ca9baad3c1e1
Aram <me@aram.sh>
2024-02-17 22:01:33 -0800

    Added Analytics and Speed Insights
    This commit adds Vercel Analytics and Speed Insights for Next.js

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,19 @@
 import { AppProps } from "next/app";
-import React from "react"; // Add missing import statement for React
 import "../styles/index.css";
 import { Toaster } from "../components/ui/sonner";
 import { SessionProvider } from "next-auth/react";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
    return (
-      <React.Fragment>
+      <>
          <SessionProvider>
             <Component {...pageProps} />
+            <Analytics />
+            <SpeedInsights />
          </SessionProvider>
          <Toaster />
-      </React.Fragment>
+      </>
    );
 }
