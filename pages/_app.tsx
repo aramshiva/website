5fb6b1f8db0700fb724196cf181339921ea0642a
Aram <me@aram.sh>
2024-02-17 22:26:40 -0800

    Ran Prettier on new forms and app layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,7 @@ import "../styles/index.css";
 import { Toaster } from "../components/ui/sonner";
 import { SessionProvider } from "next-auth/react";
 import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from '@vercel/speed-insights/next';
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
    return (
