f73897a707979fa5e8377babc197cb4899fa1f3c
Aram <me@aram.sh>
2024-03-22 23:10:17 -0700

    Add Framer Motion to animations to badges
    Added Framer Motion to the work page badges for the languages I know.

--- components/ui/badge.tsx
+++ components/ui/badge.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { motion } from "framer-motion";
 
 type Props = {
    children?: React.ReactNode;
@@ -14,15 +15,22 @@ export default function Badge({
    link,
 }: Props) {
    return (
-      <Link href={link || ""}>
-         <span
-            className={`inline-flex items-center gap-2 rounded-xl px-5 py-1 text-xs font-medium text-black ring-1 ring-inset ring-gray-200 ${className}`}
-         >
-            <svg className={`h-1.5 w-1.5`} viewBox="0 0 6 6" aria-hidden="true">
-               <circle cx={3} cy={3} r={3} fill={color} />
-            </svg>
-            {children}
-         </span>
-      </Link>
+      <motion.div
+         initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
+         <Link href={link || ""}>
+            <span
+               className={`inline-flex items-center gap-2 rounded-xl px-5 py-1 text-xs font-medium text-black ring-1 ring-inset ring-gray-200 ${className}`}
+            >
+               <svg
+                  className={`h-1.5 w-1.5`}
+                  viewBox="0 0 6 6"
+                  aria-hidden="true"
+               >
+                  <circle cx={3} cy={3} r={3} fill={color} />
+               </svg>
+               {children}
+            </span>
+         </Link>
+      </motion.div>
    );
 }
