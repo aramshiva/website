9d26f6dd3a8c7bb7886050e80a8208d698c61fc0
Aram <me@aram.sh>
2024-11-05 19:25:58 -0800

    add 404

---
+++ src/app/404/page.tsx
@@ -0,0 +1,9 @@
+export default function NotFound() {
+    return (
+        <>
+        <p>404!</p>
+        <p>looks like this site was moved or doesn't exist!</p>
+        <p>sorry about that!</p>
+        </>
+    )
+}
