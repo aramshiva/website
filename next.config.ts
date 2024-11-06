711d766bea5360184941c33a58ea3a742c12d6f6
Aram <me@aram.sh>
2024-11-05 19:16:19 -0800

    prettier

--- next.config.ts
+++ next.config.ts
@@ -5,10 +5,10 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'live.staticflickr.com',
-        port: '',
-        pathname: '**',
+        protocol: "https",
+        hostname: "live.staticflickr.com",
+        port: "",
+        pathname: "**",
       },
     ],
   },
