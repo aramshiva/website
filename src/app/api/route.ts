ecfa2860842268499b4d4b7e69a9c8a3b18edcc9
Aram <me@aram.sh>
2024-11-30 19:14:20 -0800

    Update route.ts

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,5 +1,5 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+export default function GET(_: NextApiRequest, res: NextApiResponse) {
     res.status(200).json({ city: 'Seattle', state: 'WA', country: 'US', slack_id: 'U0616280E6P', extra: 'hi! this is my website! made using next, and a bit more!' });
 }
